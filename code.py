0a716e70f2d211d0015b492f9b7cf24d52652664
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-01-04 12:03:43 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,29 +1,21 @@
-import os,time,random
-os.system('color 7')
-os.system('CLS')
-delayval = 0.2
-space = 500.0
-spa = 0
-ok = 0
-i = 0
-a = 0
+import sys,random,os
+os.system("color a")
+os.system("cls")
+def progressbar(it, prefix="", size=60, out=sys.stdout):
+  count = len(it)
+  def show(j):
+      x = int(size*j/count)
+      print("{}[{}{}] {}/{}".format(prefix, "█"*x, " "*(size-x), j, count), 
+              end='\r', file=out, flush=True)
+  show(0)
+  for i, item in enumerate(it):
+      yield item
+      show(i+1)
+  print("\n", flush=True, file=out)
 
-delayval = float(input("bekleme süresini girin > 0."))
-space = int(input("uzunlugu girin | <-> | > "))
-spa = space
-colors = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
-os.system('CLS')
-
-while a < 25:
-    while i < (spa/2)+1:
-        print("|" + "█"*ok + " "*space + "|"+ "📥")
-        space = space - 1
-        ok = ok + 1
-        if ok >= spa:
-            ok = spa
-        i = i + 0.5
-        time.sleep(delayval/10)
-        os.system('CLS')
-        os.system('color ' + random.choice(colors))
-    a = a + 1
-input()
+import time    
+num = 1
+while True:
+  for i in progressbar(range(random.randint(5, 200)), f"downloading virus [{num}/∞]: ", 50):
+      time.sleep(0.01)
+  num += 1
